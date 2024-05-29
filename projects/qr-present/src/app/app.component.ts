40d03e019c228cdc74bf51ed3bee777a3d09fea2
Kirill <kirill.nikolaev@everon.fi>
2024-05-29 17:21:32 +0300

    remove commented out code

--- projects/qr-present/src/app/app.component.ts
+++ projects/qr-present/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import * as QRCode from 'qrcode';
-import { Router } from '@angular/router';
 import { environment } from '../environments/environment';
 
 const ngrokUrl = environment.ngrokUrl;
@@ -12,38 +10,4 @@ const ngrokUrl = environment.ngrokUrl;
 })
 export class AppComponent {
   title = 'qr-present';
-  qrCodeUrl: string = '';
-  url: string = '';
-
-  constructor(private router: Router) { }
-
-  onSubmit(value: any) {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const data = {
-          input: value.inputField,
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        };
-        this.generateQRCode(data);
-      });
-    } else {
-      console.log("Geolocation is not supported by this browser.");
-    }
-  }
-
-  generateQRCode(data: any) {
-    const url = `${ngrokUrl}/display-data?input=${data.input}&latitude=${data.latitude}&longitude=${data.longitude}`;
-    QRCode.toDataURL(url)
-      .then((qrCodeUrl: string) => {
-        console.log(qrCodeUrl);
-        // You can set this qrCodeUrl to an img src to display the QR code
-        this.qrCodeUrl = qrCodeUrl;
-
-        this.router.navigate(['/student-view'], { queryParams: data });
-      })
-      .catch((err: Error) => {
-        console.error(err);
-      });
-  }
 }
